e9fc45ee11ee15a797d6fcacdf270f5c36c295db
Takaaki Uno <takaaki.uno@gmail.com>
2020-01-23 01:46:39 +0900

    fix syntax error in application.rb

--- config/application.rb
+++ config/application.rb
@@ -35,13 +35,13 @@ module Sabotter
       g.helper false
       g.test_framework :rspec,
                        model_specs: true,
-                       system: true
+                       system: true,
                        fixtures: true,
                        view_specs: false,
                        controller_specs: false,
                        routing_specs: false,
                        helper_specs: false,
-                       request_specs: false,
+                       request_specs: false
     end
   end
 end
