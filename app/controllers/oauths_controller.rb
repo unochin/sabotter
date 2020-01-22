2548fd83b5f84fcc93957f29f198c66b19d1b70a
Takaaki Uno <takaaki.uno@gmail.com>
2020-01-23 03:02:19 +0900

    add oauths_controllerを作成

---
+++ app/controllers/oauths_controller.rb
@@ -0,0 +1,43 @@
+class OauthsController < ApplicationController
+  skip_before_action :require_login, raise: false
+
+  # sends the user on a trip to the provider,
+  # and after authorizing there back to the callback url.
+  def oauth
+    login_at(params[:provider])
+  end
+
+  def callback
+    provider = 'twitter'
+    if @user = login_from(provider)
+      redirect_to user_path(@user), :notice => "Logged in from #{provider.titleize}!"
+    else
+      begin
+        @user = create_from(provider)
+        # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
+        reset_session # protect from session fixation attack
+        auto_login(@user)
+        # TODO access_token, access_token_secretは暗号化してDBに保存する
+        @user.access_token = @access_token.token
+        @user.access_token_secret = @access_token.secret
+        @user.save
+        redirect_to user_path(@user), :notice => "Logged in from #{provider.titleize}!"
+      rescue
+        redirect_to root_path, :alert => "Failed to login from #{provider.titleize}!"
+      end
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(:root, notice: 'Logged out!')
+  end
+  #example for Rails 4: add private method below and use "auth_params[:provider]" in place of
+  #"params[:provider] above.
+
+  # private
+  # def auth_params
+  #   params.permit(:code, :provider)
+  # end
+
+end
