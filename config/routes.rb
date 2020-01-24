56376d0d76497bad8320272f960d605ec915a9aa
Takaaki Uno <takaaki.uno@gmail.com>
2020-01-24 21:35:58 +0900

    modify routes.rbのロケットハッシュを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'static_pages/mission'
 
-  get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
-  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
-  post 'logout' => 'oauths#destroy', :as => :logout
+  get 'oauth/callback', to: 'oauths#callback' # for use with Github, Facebook
+  get 'oauth/:provider', as: :auth_at_provider, to: 'oauths#oauth'
+  post 'logout', as: :logout, to: 'oauths#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
