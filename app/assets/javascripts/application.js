cae878e94aae3ed06275eac7e50ddf691e108aa9
Takaaki Uno <takaaki.uno@gmail.com>
2020-01-25 14:46:57 +0900

    add bootstrap, fontawesomeの読み込み

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
 //= require rails-ujs
 //= require activestorage
-//= require_tree .
+// require turbolinks
+//= require popper
+//= require bootstrap-sprockets
