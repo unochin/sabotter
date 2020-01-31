d27a6e7f0868268a6c469868be22e3d374482733
Takaaki Uno <takaaki.uno@gmail.com>
2020-01-31 21:06:13 +0900

    modify UsersControllerのdestoryメソッドのif文を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    if current_user.destroy!
-      flash[:success] = 'アカウントを削除しました'
-      redirect_to :root
-    else
-      flash[:danger] = 'アカウントを削除できませんでした'
-      render :show
-    end
+    current_user.destroy!
+    flash[:success] = 'アカウントを削除しました'
+    redirect_to :root
   end
 
 end
