440760631e9fadd35d8783b24bab7eda5f5e0fa6
Takaaki Uno <takaaki.uno@gmail.com>
2020-01-31 19:45:51 +0900

    add アカウント削除機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
   def show
     @tasks = current_user.tasks
   end
+
+  def destroy
+    if current_user.destroy!
+      flash[:success] = 'アカウントを削除しました'
+      redirect_to :root
+    else
+      flash[:danger] = 'アカウントを削除できませんでした'
+      render :show
+    end
+  end
+
 end
