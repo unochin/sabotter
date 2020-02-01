bd9202fbcc7b026eb74311cddf64ad5a561acd88
Takaaki Uno <takaaki.uno@gmail.com>
2020-02-01 16:22:00 +0900

    modify tweet_timeのバリデーションを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,6 +8,7 @@ class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :tweet_content, presence: true, length: { maximum: 140 }
   validates :tweet_datetime, presence: true
+  validates :repeat_tweet_time, presence: true, if: :repeat?
   validates :repeat_flag, presence: true
   validates :pause_flag, presence: true
   validates :status, presence: true
