4d360813b073c072c8a04041937b0d18a9e52946
Takaaki Uno <takaaki.uno@gmail.com>
2020-02-09 23:22:04 +0900

    fix mockを使ったテストがパスするように修正
    作成したmockをrakeタスクで呼び出せていなかったのを、呼び出すように修正

--- spec/rake/auto_tweet_spec.rb
+++ spec/rake/auto_tweet_spec.rb
@@ -1,7 +1,7 @@
 require 'rake_helper'
 
 describe 'auto_tweet:auto_tweet' do
-  context '期限が過ぎた、activeかつtodoのタスクが存在する' do
+  context '期限が過ぎている・過ぎていない、休止中・休止していない、todo・done、様々な組み合わせのタスクが存在する' do
     let!(:user) { create(:user) }
     let!(:just_active_todo_repeat_task) { create(:task_for_rake, :repeat, :active, :todo, :just_tweet_datetime, :skip_validate, user: user) }
     let!(:three_minutes_after_active_todo_repeat_task) { create(:task_for_rake, :repeat, :active, :todo, :three_minutes_after_tweet_datetime, :skip_validate, user: user) }
@@ -19,8 +19,6 @@ describe 'auto_tweet:auto_tweet' do
     subject(:auto_tweet) { Rake.application['auto_tweet:auto_tweet'] }
 
     it '期限が過ぎた、activeかつtodoのタスクのみがツイートをすること' do
-      twitter_client_mock = double('Twitter client')
-      allow(twitter_client_mock).to receive(:update!)
       tasks = [
               just_active_todo_repeat_task,
               three_minutes_after_active_todo_repeat_task,
@@ -35,9 +33,20 @@ describe 'auto_tweet:auto_tweet' do
               three_minutes_after_active_done_one_time_task,
               three_minutes_before_active_done_repeat_task
               ]
-      tasks.each do |task|
-        allow(task).to receive(:twitter_client).and_return(twitter_client_mock)
+
+      # Array => ActiveRecord_Relation
+      tasks = Task.where(id: tasks.map{ |task| task.id })
+      # ActiveRecord_Relation => Array
+      tweet_tasks = tasks.have_to_tweet.to_a
+
+      twitter_client_mock = double('Twitter client')
+      allow(twitter_client_mock).to receive(:update!)
+
+      tweet_tasks.each do |tweet_task|
+        allow(tweet_task).to receive(:twitter_client).and_return(twitter_client_mock)
       end
+
+      allow(Task).to receive(:have_to_tweet).and_return(tweet_tasks)
 
       auto_tweet.invoke
 
--- spec/rake/update_tweet_datetime_spec.rb
+++ spec/rake/update_tweet_datetime_spec.rb
@@ -1,7 +1,7 @@
 require 'rake_helper'
 
 describe 'update_tweet_datetime:update_tweet_datetime' do
-  context '期限が過ぎた、activeかつtodoのタスクが存在する' do
+  context '期限が過ぎたタスクと過ぎていないタスクが存在する' do
     let!(:user) { create(:user) }
     let!(:expired_active_todo_repeat_task) { create(:task_for_rake, :repeat, :todo, :active, :three_minutes_after_tweet_datetime, :skip_validate, user: user) }
     let!(:expired_pause_todo_repeat_task) { create(:task_for_rake, :repeat, :todo, :pause, :three_minutes_after_tweet_datetime, :skip_validate, user: user) }
@@ -13,22 +13,61 @@ describe 'update_tweet_datetime:update_tweet_datetime' do
 
     subject(:update_tweet_datetime) { Rake.application['update_tweet_datetime:update_tweet_datetime'] }
 
-    it '期限が過ぎた、activeかつtodoのタスクのみが対象メソッドが呼び出すこと' do
-      allow(expired_active_todo_repeat_task).to receive(:set_next_tweet_date)
-      allow(expired_pause_todo_repeat_task).to receive(:set_next_tweet_date)
-      allow(expired_done_repeat_task).to receive(:set_next_tweet_date)
-      allow(not_expired_active_todo_repeat_task).to receive(:set_next_tweet_date)
-      allow(not_expired_pause_todo_repeat_task).to receive(:set_next_tweet_date)
-      allow(not_expired_done_repeat_task).to receive(:set_next_tweet_date)
+    it '期限が過ぎたタスクの次回ツイート時間が更新されること' do
+
+      tasks = [
+              expired_active_todo_repeat_task,
+              expired_pause_todo_repeat_task,
+              expired_done_repeat_task,
+              not_expired_active_todo_repeat_task,
+              not_expired_pause_todo_repeat_task,
+              not_expired_done_repeat_task,
+              ]
+
+      # Array => ActiveRecord_Relation
+      tasks = Task.where(id: tasks.map{ |task| task.id })
+      # ActiveRecord_Relation => Array
+      repeat_tasks = tasks.repeat.to_a
+
+      # rakeタスク実行前ツイート時間格納用
+      before_task_0_tweet_datetime = 0
+      before_task_1_tweet_datetime = 0
+      before_task_2_tweet_datetime = 0
+      before_task_3_tweet_datetime = 0
+      before_task_4_tweet_datetime = 0
+      before_task_5_tweet_datetime = 0
+      # rakeタスク実行後ツイート時間格納用
+      after_task_0_tweet_datetime = 0
+      after_task_1_tweet_datetime = 0
+      after_task_2_tweet_datetime = 0
+      after_task_3_tweet_datetime = 0
+      after_task_4_tweet_datetime = 0
+      after_task_5_tweet_datetime = 0
+
+      repeat_tasks.each_with_index do |task, index|
+        var = "before_task_#{index}_tweet_datetime"
+        value = task.tweet_datetime.year.to_s + task.tweet_datetime.month.to_s + task.tweet_datetime.day.to_s \
+                + task.tweet_datetime.hour.to_s + task.tweet_datetime.min.to_s
+        eval("#{var} = #{value}")
+      end
+
+      allow(Task).to receive(:repeat).and_return(repeat_tasks)
 
       update_tweet_datetime.invoke
 
-      expect(expired_active_todo_repeat_task).to have_received(:set_next_tweet_date).once
-      expect(expired_pause_todo_repeat_task).to have_received(:set_next_tweet_date).at_most(0).times
-      expect(expired_done_repeat_task).to have_received(:set_next_tweet_date).at_most(0).times
-      expect(not_expired_active_todo_repeat_task).to have_received(:set_next_tweet_date).at_most(0).times
-      expect(not_expired_pause_todo_repeat_task).to have_received(:set_next_tweet_date).at_most(0).times
-      expect(not_expired_done_repeat_task).to have_received(:set_next_tweet_date).at_most(0).times
+      repeat_tasks.each_with_index do |task, index|
+        var = "after_task_#{index}_tweet_datetime"
+        value = task.tweet_datetime.year.to_s + task.tweet_datetime.month.to_s + task.tweet_datetime.day.to_s \
+                + task.tweet_datetime.hour.to_s + task.tweet_datetime.min.to_s
+        eval("#{var} = #{value}")
+      end
+
+      expect(before_task_0_tweet_datetime != after_task_0_tweet_datetime).to eq true
+      expect(before_task_1_tweet_datetime != after_task_1_tweet_datetime).to eq true
+      expect(before_task_2_tweet_datetime != after_task_2_tweet_datetime).to eq true
+      expect(before_task_3_tweet_datetime == after_task_3_tweet_datetime).to eq true
+      expect(before_task_4_tweet_datetime == after_task_4_tweet_datetime).to eq true
+      expect(before_task_5_tweet_datetime == after_task_5_tweet_datetime).to eq true
     end
   end
 end
